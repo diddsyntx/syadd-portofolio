8af059484d10d0b44785adf24dee7d62c14cd482
Syadd <sadisjawawx097@gmail.com>
2026-06-19 11:02:04 +0700

    Add files via upload

--- GAME/warrior2.js
+++ GAME/warrior2.js
@@ -2,13 +2,12 @@ document.addEventListener("keydown", (e) => {
   if (e.code === "ArrowRight" && !warrior2.isMovingRight) {
     warrior2.isMovingRight = true;
     warrior2.isFacingLeft = false;
-    updateImageByMovement();
+    updateImageByMovementEnemy();
   }
 
   if (e.code === "ArrowLeft" && !warrior2.isMovingLeft) {
     warrior2.isMovingLeft = true;
     warrior2.isFacingLeft = true;
-    updateImageByMovement();
+    updateImageByMovementEnemy();
   }
-  console.log(e);
 });
